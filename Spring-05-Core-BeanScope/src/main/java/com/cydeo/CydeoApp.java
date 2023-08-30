14c935168cfd33ca9e532faa4ccac954eb8f5d2c
Betul Zora <zorabtl@gmail.com>
2023-08-30 14:55:26 -0500

    Finished Singleton and Prototype. Then covered Eager vs Lazy instantiation

--- Spring-05-Core-BeanScope/src/main/java/com/cydeo/CydeoApp.java
+++ Spring-05-Core-BeanScope/src/main/java/com/cydeo/CydeoApp.java
@@ -15,11 +15,30 @@ public class CydeoApp {
 
         ApplicationContext context = new AnnotationConfigApplicationContext(ProjectConfig.class);
 
-        CommentService commentService = context.getBean(CommentService.class);
-        commentService.publishComment(comment);
+        CommentService cs1 = context.getBean(CommentService.class);
+       // CommentService cs2 = context.getBean(CommentService.class);
 
 
+        // notice that these have the same memory address
+        //System.out.println(cs1);
+       // System.out.println(cs2);
 
+       // System.out.println(cs1==cs2);
+
+        // Singleton is used by Default
+        // To have different objects for the reference, need to use Prototype Scope
+
+        // when the service is marked with @Scope("prototype") they are different objects
+
+
+        // after the hello line has been added to constructor, notice that with prototype, the constructor is called twice
+
+        // When you return back to singleton and create the context and disable everything else,
+        // the constructor of CommentService is called when context is created
+        // this is EAGER INSTANTIATION
+
+        // Turn it lazy with Prototype or with @Lazy
+        // It doesn't call the constructor at context creation, but when the bean is referred to.
 
     }
 }
--- Spring-05-Core-BeanScope/src/main/java/com/cydeo/service/CommentService.java
+++ Spring-05-Core-BeanScope/src/main/java/com/cydeo/service/CommentService.java
@@ -4,9 +4,15 @@ import com.cydeo.model.Comment;
 import com.cydeo.proxy.CommentNotificationProxy;
 import com.cydeo.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+//@Scope("prototype")
+// @Scope(BeanDefinition.SCOPE_PROTOTYPE)
+@Lazy
 public class CommentService {
 
     // NOTE:
@@ -21,6 +27,7 @@ public class CommentService {
     public CommentService(CommentRepository commentRepository, @Qualifier("PUSH") CommentNotificationProxy commentNotificationProxy) {
         this.commentRepository = commentRepository;
         this.commentNotificationProxy = commentNotificationProxy;
+        System.out.println("Hello, comment service constructor has been called");
     }
 
     public void publishComment(Comment comment){
