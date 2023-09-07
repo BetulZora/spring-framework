df03073af8fbbccd17469489df4e0aabdc20f180
Betul Zora <zorabtl@gmail.com>
2023-09-07 12:51:35 -0500

    Learned how to navigate subfolders under resources.static

--- Spring-07-SpringMVC-Controller/src/main/java/com/cydeo/controller/HomeController.java
+++ Spring-07-SpringMVC-Controller/src/main/java/com/cydeo/controller/HomeController.java
@@ -3,18 +3,28 @@ package com.cydeo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+@Controller // this stereotype annotation marks the controller
 public class HomeController {
 
-    @RequestMapping("/home")
+    @RequestMapping("/home") // The RequestMapping Annotation associcates the endpoint or HTTP request with a method
     public String home(){
-        return "home.html";
+        return "home.html"; // Return the html document name as string with display instructions
     }
 
     @RequestMapping("/welcome")
     public String welcome(){
         return "welcome.html";
     }
+    @RequestMapping
+    public String welcome2(){
+        return "welcome.html";
+    }
 
+//
+//    // These have been moved to UserController.java
+//    @RequestMapping("/user")
+//    public String userInfo(){
+//        return "user/userinfo.html";
+//    }
 
 }
