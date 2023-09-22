0d9828ef7e3cbd43b6c59cc4c27596d634028093
Betul Zora <zorabtl@gmail.com>
2023-09-22 13:39:39 -0500

    learning usage of .properties

---
+++ Spring-09-Spring-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
@@ -0,0 +1,37 @@
+package com.cydeo.controller;
+
+import com.cydeo.enums.Gender;
+import com.cydeo.model.Mentor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("/mentor")
+public class MentorController {
+
+
+    // this end point is actually joint /mentor/list
+    @RequestMapping("/list")
+    public String mentorMethod(Model model){
+        Mentor mentor1 = new Mentor("Mike", "Smith", 45, Gender.MALE);
+        Mentor mentor2 = new Mentor("Tom", "Hanks", 65, Gender.MALE);
+        Mentor mentor3 = new Mentor("Amy", "Bryan", 25, Gender.FEMALE);
+        model.addAttribute("mentor1", mentor1 );
+        model.addAttribute("mentor2", mentor2 );
+        model.addAttribute("mentor3", mentor3 );
+
+        List<Mentor> mentors = new ArrayList<>();
+        mentors.add(mentor1);
+        mentors.add(mentor2);
+        mentors.add(mentor3);
+
+        model.addAttribute("mentors", mentors);
+        return "mentor/mentorList";
+    }
+
+
+}
---
+++ Spring-09-Spring-Thymeleaf/src/main/java/com/cydeo/model/Mentor.java
@@ -0,0 +1,14 @@
+package com.cydeo.model;
+
+import com.cydeo.enums.Gender;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class Mentor {
+
+    public String firstName, lastName;
+    public int age;
+    public Gender gender;
+
+
+}
