847e4d9959dc201ed3322f148580ae10ff17bc8e
Betul Zora <zorabtl@gmail.com>
2023-10-17 17:29:49 -0500

    Initialize H2 DB with a schema

--- Spring-10-ORM-TableCreation1/src/main/java/com/cydeo/SpringORMApplication.java
+++ Spring-10-ORM-TableCreation1/src/main/java/com/cydeo/SpringORMApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Spring10OrmTableCreation1Application {
+public class SpringORMApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Spring10OrmTableCreation1Application.class, args);
+        SpringApplication.run(SpringORMApplication.class, args);
     }
 
 }
