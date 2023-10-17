6b2abf3a0b52b575981b325ec18661f4ccb63ec4
Betul Zora <zorabtl@gmail.com>
2023-10-17 16:30:33 -0500

    Started ORM

---
+++ Spring-10-ORM-TableCreation1/src/main/java/com/cydeo/springormtablecreation/Spring10OrmTableCreation1Application.java
@@ -0,0 +1,13 @@
+package com.cydeo.springormtablecreation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring10OrmTableCreation1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring10OrmTableCreation1Application.class, args);
+    }
+
+}
---
+++ Spring-10-ORM-TableCreation1/src/test/java/com/cydeo/springormtablecreation/Spring10OrmTableCreation1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.springormtablecreation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring10OrmTableCreation1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
