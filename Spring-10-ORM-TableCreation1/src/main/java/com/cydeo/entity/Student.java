a528056eaed27848ac6484eefad2181a4500eaf7
Betul Zora <zorabtl@gmail.com>
2023-10-20 11:10:26 -0500

    @Table @Column and @Transient

--- Spring-10-ORM-TableCreation1/src/main/java/com/cydeo/entity/Student.java
+++ Spring-10-ORM-TableCreation1/src/main/java/com/cydeo/entity/Student.java
@@ -1,22 +1,45 @@
 package com.cydeo.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 
 @Entity
+@Table(name="students")
+// use @Table(name="NewName") to change the datatable name
 public class Student {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    // use @Column(name="newName") to rename a column
+    // the DB column will adjust for camelcase to new_name
+    @Column(name="studentFirstName") // will adjust for camel case in DB
     private String firstName;
     private String lastName;
     private String email;
 
+
+    // @Transient prevents Spring from including the field in the DB
+    @Transient
+    private String city;
+
+
+    // These data types introduced in Java8. Rename columns
+    @Column(name="DATE")
+    private LocalDate birthDate;
+    @Column(name="TIME")
+    private LocalTime birthTime;
+    @Column(name="TIMESTAMP")
+    private LocalDate birthDateTime;
+
     // by default, the table created will take camel case, convert to lowercase separated by "_"
     // firstName becomes first_name
+
+
 
 }
