72e045674aeb78a0188760cfeff5f49dfadd64ee
Betul Zora <zorabtl@gmail.com>
2023-10-19 19:01:03 -0500

    learned to use @Id     @GeneratedValue(strategy = GenerationType.IDENTITY)

--- Spring-10-ORM-TableCreation1/src/main/java/com/cydeo/entity/Student.java
+++ Spring-10-ORM-TableCreation1/src/main/java/com/cydeo/entity/Student.java
@@ -1,15 +1,22 @@
 package com.cydeo.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Student {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String firstName;
     private String lastName;
     private String email;
+
+    // by default, the table created will take camel case, convert to lowercase separated by "_"
+    // firstName becomes first_name
 
 }
