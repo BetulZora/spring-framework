e400555c8f030d3b2a3766090e71ea997c6e952c
Betul Zora <zorabtl@gmail.com>
2023-10-25 21:10:44 -0500

    Set up the DataGenerator

--- Spring-11-ORM-Mapping-OneToOne/src/main/java/com/cydeo/respository/EmployeeRepository.java
+++ Spring-11-ORM-Mapping-OneToOne/src/main/java/com/cydeo/respository/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.cydeo.respository;
 
 import com.cydeo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
