9a8fcd8c6fae115229c22d7df25f75f8c88138cf
Betul Zora <zorabtl@gmail.com>
2023-11-01 18:13:18 -0500

    Added more entities

---
+++ Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/entity/Ticket.java
@@ -0,0 +1,27 @@
+package com.cydeo.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToOne;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Ticket extends BaseEntity{
+
+    @Column(columnDefinition = "TIMESTAMP")
+    private LocalDateTime dateTime;
+    private Integer seatNumber;
+    private Integer rowNumber;
+
+    @ManyToOne (fetch = FetchType.LAZY)
+    private MovieCinema movie_cinema_id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+}
