9e82cff1c11fbbb8bada17d8d2cca8a71426aaea
Betul Zora <zorabtl@gmail.com>
2023-11-02 16:15:06 -0500

    Practiced Derived Queries in the RegionRepository

--- Spring-14-ORM-Queries/src/main/java/com/cydeo/bootstrap/DataGenerator.java
+++ Spring-14-ORM-Queries/src/main/java/com/cydeo/bootstrap/DataGenerator.java
@@ -2,7 +2,9 @@ package com.cydeo.bootstrap;
 
 import com.cydeo.repository.RegionRepository;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
+@Component
 public class DataGenerator implements CommandLineRunner {
 
     private final RegionRepository regionRepository;
@@ -16,7 +18,11 @@ public class DataGenerator implements CommandLineRunner {
 
         // Display all regions in Canada using derived queries in the repository
         System.out.println("---------------------------+++++++++Region Start");
-        System.out.println(regionRepository.findByCountry("Canada"));
+        System.out.println("In the DATA GENERATOR:" + regionRepository.findByCountry("Canada"));
+        System.out.println(regionRepository.findDistinctByCountry("Canada"));
+        System.out.println(regionRepository.findByCountryContaining("United"));
+        System.out.println(regionRepository.findByCountryContainingOrderByRegion("United"));
+        System.out.println(regionRepository.findTop2ByCountry("Canada"));
 
         System.out.println("---------------------------+++++++++Region End");
 
--- Spring-14-ORM-Queries/src/main/java/com/cydeo/repository/RegionRepository.java
+++ Spring-14-ORM-Queries/src/main/java/com/cydeo/repository/RegionRepository.java
@@ -10,4 +10,16 @@ public interface RegionRepository extends JpaRepository <Region, Long> {
     // Display all regions in Canada using derived queries
     List<Region> findByCountry(String country);
 
+    // Display all regions in Canada without duplicates
+    List<Region> findDistinctByCountry(String country);
+
+    // Display all regions with country name includes 'United'
+    List<Region> findByCountryContaining(String prefix);
+
+    // Display all regions with country name including 'United' in order
+    List<Region> findByCountryContainingOrderByRegion(String prefix);
+
+    // Display top 2 regions in Canada
+    List<Region> findTop2ByCountry(String country);
+
 }
