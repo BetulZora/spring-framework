c6cfdddaa753cedaac69c4eb15c436caaef8ba6c
Betul Zora <zorabtl@gmail.com>
2023-12-12 14:39:23 -0600

    Built endpoints that consume thirdparty APIs using the restTemplate.getForEntity() and restTemplate.getForObject() methods. There is one more method that I will learn soon

--- Spring-17-REST-ConsumingApis/src/main/java/com/cydeo/Spring17RestConsumingApisApplication.java
+++ Spring-17-REST-ConsumingApis/src/main/java/com/cydeo/Spring17RestConsumingApisApplication.java
@@ -2,6 +2,8 @@ package com.cydeo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class Spring17RestConsumingApisApplication {
@@ -10,4 +12,10 @@ public class Spring17RestConsumingApisApplication {
         SpringApplication.run(Spring17RestConsumingApisApplication.class, args);
     }
 
+
+    // RestTemplate, if used, needs to be used as a Bean
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
 }
--- Spring-17-REST-ConsumingApis/src/main/java/com/cydeo/controller/Consume_RestTemplate.java
+++ Spring-17-REST-ConsumingApis/src/main/java/com/cydeo/controller/Consume_RestTemplate.java
@@ -1,4 +1,55 @@
 package com.cydeo.controller;
 
+import com.cydeo.dto.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+
+
+@RestController
+@RequestMapping("/users")
 public class Consume_RestTemplate {
+
+    // define the URI that will be consumed API endpoint
+    private final String URI = "https://jsonplaceholder.typicode.com/users";
+    // inject the RestTemplate bean
+    private final RestTemplate restTemplate;
+
+    public Consume_RestTemplate(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    // use Get mapping when consuming
+    @GetMapping
+    public User[] readAllUsers(){
+
+        // can use RestTemplate Methods: getForEntity(), getForObject()
+
+        // getForEntity takes as parameters the URI of the endpoint and what it will turn in to
+        // Cannot use List, USE ARRAY
+        // will call the endpoint, then will convert it to the specified type
+        ResponseEntity<User[]> responseEntity = restTemplate.getForEntity(URI, User[].class);
+
+        return responseEntity.getBody();
+
+        // thus, going to my endpoint localhost:9090/users will product the same output as the end point in URI
+        // conceivably, you can use that information and generate a different response
+
+    }
+
+    @GetMapping("{id}")
+    public Object readUser(@PathVariable("id") Integer id){
+        // second RestTemplate method: getForObject()
+
+        // getForObject takes as parameters
+
+
+        String URL = URI+"/{id}";
+        return restTemplate.getForObject(URL, Object.class, id);
+
+    }
 }
