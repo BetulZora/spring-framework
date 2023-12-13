9084b78db3ee70e02d6657f71cd83df8f647299d
Betul Zora <zorabtl@gmail.com>
2023-12-13 14:12:06 -0600

    learned about the exchange method of RestTemplate for when headers need to be sent in API call

--- Spring-17-REST-ConsumingApis/src/main/java/com/cydeo/controller/Consume_RestTemplate.java
+++ Spring-17-REST-ConsumingApis/src/main/java/com/cydeo/controller/Consume_RestTemplate.java
@@ -1,6 +1,8 @@
 package com.cydeo.controller;
 
 import com.cydeo.dto.User;
+import org.springframework.http.*;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 
 
 @RestController
@@ -27,7 +30,7 @@ public class Consume_RestTemplate {
     @GetMapping
     public User[] readAllUsers(){
 
-        // can use RestTemplate Methods: getForEntity(), getForObject()
+        // can use RestTemplate Methods: getForEntity(), getForObject(), exchange()
 
         // getForEntity takes as parameters the URI of the endpoint and what it will turn in to
         // Cannot use List, USE ARRAY
@@ -45,11 +48,34 @@ public class Consume_RestTemplate {
     public Object readUser(@PathVariable("id") Integer id){
         // second RestTemplate method: getForObject()
 
-        // getForObject takes as parameters
-
-
+        // getForObject takes as parameters: 1)the URL endpoint with the path param concatenated
+        // 2) Object.class
+        // 3) the information to place into the path param dynamically
         String URL = URI+"/{id}";
         return restTemplate.getForObject(URL, Object.class, id);
+
+    }
+
+    @GetMapping("/test")
+    public ResponseEntity<Object> consumeFromDummyApi(){
+        // third RestTemplate method: exchange()
+
+        // especially use when credentials or headers are needed
+        // exchange method takes as param: end point, HttpMethod (of the endpoint), HttpEntity, Object.class
+
+        // first populate a HttpHeaders object.
+        // second use the HttpHeaders object to create an HttpEntity<String> object.
+        // third send the HttpEntity<String> object in the ResponseEntity<Object> object.
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        headers.set("app-id","6298ebfecd0551211fce37a6");
+
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        ResponseEntity<Object> response = restTemplate.exchange("https://dummyapi.io/data/v1/user?limit=10", HttpMethod.GET,entity,Object.class);
+
+        return response;
 
     }
 }
